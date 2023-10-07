65170f791b8b33d4eda0856d65102b041ad3ae72
Shipra Suman <sumanshipra203@gamil.com>
2023-10-07 17:06:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+// import './App.css';
+import LoginSignup from './components/LoginSignup/log';
+
+// import React from 'react';
+
+function App() {
+  return (
+    <>
+      <LoginSignup />
+
+    </>
+  );
+}
+
+export default App;
